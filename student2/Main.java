75636ad0b61303754475bfa6bab1bbd47115113f
Sujeong Kim <62699938+sujeong816@users.noreply.github.com>
2023-10-18 10:32:44 +0900

    Merge branch 'master' into four

--- student2/Main.java
+++ student2/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Two.print();
         System.out.println("one 202014115");
+        Three.print();
         Four.print();
     }
 }
